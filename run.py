052ddd8d4d2010e863f799497094c61f14300766
Hayk <haykooo3@gmail.com>
2024-07-09 21:45:05 +0300

    commit run update

--- run.py
+++ run.py
@@ -1,10 +1,3 @@
 from app import grid_test, pack_test, place_test
 
-# print(password(12, symbol=False))
-# pass_tkinter()
-
-
-grid_test.button3.configure(fg_color=grid_test.button1.cget("fg_color"))
-
-# pack_test.app.mainloop()
 grid_test.app.mainloop()
